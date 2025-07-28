0ab29a0469b720901106289076e78000e4bf8b4f
Syamantak Mondal <mondalsyamantak234@gmail.com>
2025-07-29 01:25:25 +0530

    removed proxy

--- vite.config.js
+++ vite.config.js
@@ -5,23 +5,6 @@ import { defineConfig } from "vite"
 
 // https://vite.dev/config/   http://localhost:5173
 export default defineConfig({
-  server: {
-    // proxy: {
-    //   '/api': {
-    //     target: 'https://todo-backend-4hmo.onrender.com',
-    //     changeOrigin: true,
-    //     secure: true, // set to false if backend has self-signed HTTPS
-    //   },
-    // },
-    proxy: {
-      '/api': {
-        target: 'http://localhost:8000',
-        changeOrigin: true,
-        secure: true, // set to false if backend has self-signed HTTPS
-      },
-    },
-  },
-
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
